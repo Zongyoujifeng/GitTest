3e7deb3e541cff766f0cd8f90dd8b8a325ad44d2
Zongyoujifeng <m18797818385@163.com>
2021-01-13 11:05:36 +0800

    GitTest

---
+++ testgit/src/TestGit.java
@@ -0,0 +1,12 @@
+/**
+ * @author 10451
+ * @version 1.0
+ * @className: TestGit
+ * @description: 测试Git
+ * @date 2021/1/13 11:02
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("Hello GitTest");
+    }
+}
